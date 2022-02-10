9d45196bf67fa1455c871e939fa8084e1af8aa1b
marcelmanzel <marcel.manzel@oxid-esales.com>
2022-02-10 11:38:24 +0100

    add module files like metadata.php and composer.json

---
+++ metadata.php
@@ -0,0 +1,31 @@
+<?php
+
+$sMetadataVersion = '2.1';
+
+$aModule = [
+    'id'           => 'myvendor_mytestmodule',
+    'title'        => 'Test metadata controllers feature',
+    'description'  => '',
+    'thumbnail'    => 'picture.png',
+    'version'      => '2.1',
+    'author'       => 'OXID eSales AG',
+//    'controllers'  => [
+//        'myvendor_mytestmodule_MyModuleController' => MyVendor\mytestmodule\MyModuleController::class,
+//        'myvendor_mytestmodule_MyOtherModuleController' => MyVendor\mytestmodule\MyOtherModuleController::class,
+//    ],
+    'blocks' => [
+        [
+            //'theme' => 'flow',
+            'template' => 'page/details/inc/productmain.tpl',
+            'block' => 'details_productmain_title',
+            'file' => '/views/blocks/productmain.tpl'
+        ],
+    ]
+//    'templates' => [
+//        'mytestmodule.tpl' => 'mytestmodule/mytestmodule.tpl',
+//        'mytestmodule_other.tpl' => 'mytestmodule/test_module_controller_routing_other.tpl'
+//    ],
+//    'smartyPluginDirectories' => [
+//        'Smarty/PluginDirectory'
+//    ],
+];
